e7e84ce9925a915623707dfec28616ec5862902e
Jarrian <glorianagojar@gmail.com>
2022-09-26 14:52:25 +0800

    icon and data fetch

---
+++ app.js
@@ -0,0 +1,19 @@
+const endpoint = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.json';
+let covidLatest = {};
+
+const getJSON = async url => {
+  const response = await fetch(url);
+  if(!response.ok) // check if response worked (no 404 errors etc...)
+    throw new Error(response.statusText);
+
+  const data = response.json(); // get JSON from the response
+  return data; // returns a promise, which resolves to this data value
+}
+
+console.log('Fetching data...');
+
+getJSON(endpoint).then(data => {
+  covidLatest = data;
+  }).catch(error => {
+  console.error(error);
+});
